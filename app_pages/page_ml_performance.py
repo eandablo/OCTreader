ea915f457805969443009f235925eeba12d2fd8b
Efren Andablo <eandablo@gmail.com>
2024-03-27 19:21:22 +0000

    add classification report to page_ml_performance

--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -5,6 +5,18 @@ from matplotlib.image import imread
 import joblib
 
 
+def write_classification_report(report_dict):
+    labels = list(report_dict.keys())
+    column_values = ['precision', 'recall', 'support', 'f1-score']
+    labels = labels[:4]
+    df = pd.DataFrame(columns=column_values)
+    for label in labels:
+        temp_dict = report_dict[label]
+        df2 = pd.DataFrame(data=temp_dict, index=[label])
+        df = df.append(df2.round(2))
+    return df
+
+
 def page_ml_performance_metrics():
     version = 'v3'
 
@@ -37,7 +49,7 @@ def page_ml_performance_metrics():
     st.write('### Loss and Occuracy of Train Dataset  \n')
     evaluation = joblib.load(f"outputs/{version}/evaluation.pkl")
     evaluation = ['%.2f' % elem for elem in evaluation]
-    st.dataframe(pd.DataFrame(data=evaluation, columns=['Values',], index=['Loss', 'Accuracy']))
+    st.write(pd.DataFrame(data=evaluation, columns=['Values',], index=['Loss', 'Accuracy']))
     st.write("---")
 
     st.write(
@@ -54,4 +66,18 @@ def page_ml_performance_metrics():
     with col3:
         confussion_mat_test = plt.imread(f"outputs/{version}/confussion_matrix_train.png")
         st.image(confussion_mat_test, caption='Confussion Matrix for Test Set')
+
+    st.write('---')
+    st.write('### Datasets Classification Reports')
+    
+    report_train = joblib.load(f'outputs/{version}/class_report_train.pkl')
+    report_val = joblib.load(f'outputs/{version}/class_report_val.pkl')
+    report_test = joblib.load(f'outputs/{version}/class_report_test.pkl')
+    st.write('')
+    df = write_classification_report(report_train)
+    st.write(df)
+    df = write_classification_report(report_val)
+    st.write(df)
+    df = write_classification_report(report_test)
+    st.write(df)
 
