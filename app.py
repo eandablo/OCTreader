0665a277e6295a54839fe32d55ecf9d37e37e16e
Efren Andablo <eandablo@gmail.com>
2024-03-27 15:43:59 +0000

    add page_ml_performance with confussion matrices

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from app_pages.multipage import MultiPage
 from app_pages.page_introduction import page_introduction_body
 from app_pages.page_data_image_visualiser import data_image_visualiser_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
+from app_pages.page_ml_performance import page_ml_performance_metrics
+
 
 app = MultiPage(app_name="OCT macular inspector")
 
@@ -10,4 +12,5 @@ app = MultiPage(app_name="OCT macular inspector")
 app.add_page("Quick Project Summary", page_introduction_body)
 app.add_page("OCT Image Visualiser", data_image_visualiser_body)
 app.add_page("Project Hypothesis", page_project_hypothesis_body)
+app.add_page("Model Performance", page_ml_performance_metrics)
 app.run()
---
+++ app_pages/page_ml_performance.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.image import imread
+# from src.machine_learning.evaluate_clf import load_test_evaluation
+
+
+def page_ml_performance_metrics():
+    version = 'v3'
+
+    st.write(
+        f"### Train, Validation and Test Dataset: Counts per label  \n"
+        f"As evident in the figure below, the datasets are well balanced"
+    )
+
+    labels_distribution = plt.imread(f"outputs/{version}/labels_distribution.png")
+    st.image(labels_distribution, caption='Labels Distribution on Train, Validation and Test Sets')
+    st.write("---")
+
+
+    st.write(
+        f"### Model Training History  \n"
+        f"The figures below show the model occuracy and loss during training. "
+        f"The monotonous behaviour of the curves and sumilarity of absolute "
+        f"values show no overfitting during training"
+    )
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
+        st.image(model_acc, caption='Model Training Accuracy')
+    with col2:
+        model_loss = plt.imread(f"outputs/{version}/model_training_losses.png")
+        st.image(model_loss, caption='Model Training Losses')
+    st.write("---")
+
+
+    st.write(
+        f"### Model Performance  \n"
+        f"The figures below show the confussion matrices for all datasets. "
+    )
+    col1, col2, col3 = st.beta_columns(3)
+    with col1:
+        confussion_mat_train = plt.imread(f"outputs/{version}/confussion_matrix_train.png")
+        st.image(confussion_mat_train, caption='Confussion Matrix for Training Set')
+    with col2:
+        confussion_mat_val = plt.imread(f"outputs/{version}/confussion_matrix_train.png")
+        st.image(confussion_mat_val, caption='Confussion Matrix for Validation Set')
+    with col3:
+        confussion_mat_test = plt.imread(f"outputs/{version}/confussion_matrix_train.png")
+        st.image(confussion_mat_test, caption='Confussion Matrix for Test Set')
